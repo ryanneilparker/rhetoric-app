b2c8ebd1e69f5cd76561fda16d4658b737e63f2f
= <ryanneilparker@gmail.com>
2023-12-05 18:30:55 +0200

    Add basic api call

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,37 @@
 <script>
-  // potentially put mvp api calls here...
+  let blogContent = "Oops, looks like there's nothing to see here...";
+
+  async function handleSubmit(event) {
+    const formData = new FormData(event.target);
+    const prompt = formData.get("prompt");
+
+    try {
+      const response = await fetch("http://localhost:8000/generate_blog", {
+        method: "POST",
+        body: prompt,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        blogContent = data.blog_content; // Update blogContent variable with new content
+      } else {
+        console.error("Failed to generate blog content");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 </script>
 
 <main>
   <section id="app">
     <div id="display">
-      <p>Renedered blog content goes here !!!</p>
+      <p>{blogContent}</p>
     </div>
-    <div id="prompt">
-      <form action="">
-        <input type="text" name="text" id="text" />
-        <input type="submit" name="submit" id="submit" />
+    <div id="prompter">
+      <form on:submit|preventDefault={handleSubmit}>
+        <input type="text" name="prompt" id="prompt" />
+        <button type="submit">Submit</button>
       </form>
     </div>
   </section>
