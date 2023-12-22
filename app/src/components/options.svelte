938c1df3275455c2fa7a8f2618b2846228819408
ryanneilparker <ryanneilparker@gmail.com>
2023-12-22 17:39:11 +0200

    Add options skeleton

---
+++ app/src/components/options.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<section id="options">
+    <input type="button" value="Tone">
+    <input type="button" value="Structure">
+    <input type="button" value="Style">
+    <input type="button" value="Voice">
+    <input type="button" value="Purpose">
+</section>
+
+<style>
+    #options {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+        gap: 1rem;
+
+        padding: 1rem;
+
+        /* debug */
+        border: 1px solid black;
+    }
+</style>
---
+++ app/src/routes/editor/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Options from "../../components/options.svelte";
+</script>
+
+<section>
+    <Options />
+</section>
+
+<style>
+
+</style>
