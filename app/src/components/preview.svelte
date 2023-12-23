a81254635b1890a2df191ca34839232ff8c70de0
ryanneilparker <ryanneilparker@gmail.com>
2023-12-23 11:52:54 +0200

    Add preview skeleton

---
+++ app/src/components/preview.svelte
@@ -0,0 +1,43 @@
+<script>
+  let htmlContent = `
+  <h1>Heading 1</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quibusdam unde libero optio quis quasi eveniet culpa molestiae, expedita corporis laudantium nulla quisquam corrupti doloremque repellendus maiores eos, ipsa reprehenderit?</p>
+  <h2>Heading 2</h2>
+  <ul>
+    <li>one</li>
+    <li>two</li>
+    <li>three</li>
+  </ul>
+  <h3>Heading 3</h3>
+  <ol>
+    <li>one</li>
+    <li>two</li>
+    <li>three</li>
+  </ol>
+  `
+</script>
+
+<section id="preview">
+  <div class="content">
+    {@html htmlContent}
+  </div>
+</section>
+  
+  <style>
+    #preview {
+      display: grid;
+
+      margin-top: 1rem;
+      padding: 2rem 4rem 2rem 4rem;
+
+      background-color: grey;
+
+      /* debug */
+      border: 1px solid green;
+    }
+
+    .content {
+      padding: 1rem;
+      background-color: white;
+    }
+  </style>
--- app/src/routes/editor/+page.svelte
+++ app/src/routes/editor/+page.svelte
@@ -1,14 +1,19 @@
 <script>
     import Options from "../../components/options.svelte";
     import Prompter from "../../components/prompter.svelte";
+    import Preview from "../../components/preview.svelte";
 </script>
 
-<section>
+<section id="editor">
+
     <Options />
     <Prompter />
-
+    <Preview />
 </section>
 
 <style>
-
+    #editor {
+        display: grid;
+        grid-template-rows: 1fr 1fr 8fr;
+    }
 </style>
