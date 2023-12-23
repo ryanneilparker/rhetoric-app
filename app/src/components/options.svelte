88846590971a4b9a2005470de2d5b8e208bb753f
ryanneilparker <ryanneilparker@gmail.com>
2023-12-23 12:02:16 +0200

    Add dropdowns to options componenet

--- app/src/components/options.svelte
+++ app/src/components/options.svelte
@@ -3,11 +3,41 @@
 </script>
 
 <section id="options">
-    <button>Tone</button>
-    <button>Style</button>
-    <button>Structure</button>
-    <button>Voice</button>
-    <button>Purpose</button>
+    <select id="tone" name="tone">
+        <option disabled selected>Select Tone</option>
+        <option value="Casual">Casual</option>
+        <option value="Formal">Formal</option>
+        <option value="Professional">Professional</option>
+    </select>
+
+    <select id="structure" name="structure">
+        <option disabled selected>Select Structure</option>
+        <option value="Listicle">Listicle</option>
+        <option value="How-To/Guide">How-To/Guide</option>
+        <option value="Opinion/Editorial">Opinion/Editorial</option>
+    </select>
+
+    <select id="style" name="style">
+        <option disabled selected>Select Style</option>
+        <option value="Conversational">Conversational</option>
+        <option value="Informative">Informative</option>
+        <option value="Engaging">Engaging</option>
+    </select>
+
+    <select id="voice" name="voice">
+        <option disabled selected>Select Voice</option>
+        <option value="Authoritative">Authoritative</option>
+        <option value="Friendly">Friendly</option>
+        <option value="Educational">Educational</option>
+    </select>
+
+    <select id="purpose" name="purpose">
+        <option disabled selected>Select Purpose</option>
+        <option value="Educational">Educational</option>
+        <option value="Instructional">Instructional</option>
+        <option value="Entertainment">Entertainment</option>
+        <option value="Thought Leadership">Thought Leadership</option>
+    </select>
 </section>
 
 <style>
